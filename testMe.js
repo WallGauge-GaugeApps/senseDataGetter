f472daaf8223e0b548065de0f082a0a9a106dd4c
John Rucker <JHRucker@outlook.com>
2020-07-01 12:57:57 -0500

    .

--- testMe.js
+++ testMe.js
@@ -3,6 +3,8 @@ const actObj = require('./actObj.json');
 
 const sense = new Sense(actObj.email, actObj.password, false);
 var solarPowered = null;
+var reconnectCont = 0
+const reconnet = 6
 sense.on('authenticated', () => {
     console.log('We are logged in and authenticated!')
     console.log('Reading Trends...');
@@ -12,22 +14,27 @@ sense.on('authenticated', () => {
             console.log('This week ' + solarPowered + '% of the power was from renewable energy.');
             sense.openWebSocket();
         })
-    
-    setInterval(()=>{
-        sense.getTrends('week')
-        .then((data) => {
-            solarPowered = data.solar_powered
-            console.log('Update. This week ' + solarPowered + '% of the power was from renewable energy.');
-        })
-    },10*60*1000)
 
-    setInterval(()=>{
-        console.log('Reconnecting to sense.com...')
-        sense.closeWebSoc();
-        sense.authenticate();
-        sense.openWebSocket();
-    },61*60*1000)
-})
+    setInterval(() => {
+        reconnectCont++
+        if (reconnectCont < reconnet) {
+            sense.getTrends('week')
+                .then((data) => {
+                    solarPowered = data.solar_powered
+                    console.log('Update. This week ' + solarPowered + '% of the power was from renewable energy.');
+                })
+                .catch((err)=>{
+                    console.error('Error trying to getTrends from sense.com', err)
+                });
+        } else {
+            reconnectCont = 0;
+            console.log('-------- Reconnecting to sense.com ---------');
+            sense.closeWebSoc();
+            sense.authenticate();
+            sense.openWebSocket();
+        };
+    }, 10 * 60 * 1000);
+});
 
 sense.on('power', () => {
     console.log((new Date()).toLocaleTimeString() +
